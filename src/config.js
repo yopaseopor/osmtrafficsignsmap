7117f69dcc1fb67cb3d9280f95736f65f7edbd7e
yopaseopor <yopaseopor@gmail.com>
2022-10-31 13:56:52 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -213,7 +213,7 @@ style: function (feature) {
 
 		},
 		{
-			group: 'Test',
+			group: 'Test2',
 			title: 'ES:R2',
 			query: '(nwr[~"^traffic_sign"~"ES:R2"]({{bbox}});node(w););out meta;',
 			iconSrc: 'https://raw.githubusercontent.com/yopaseopor/beta_preset_josm/master/ES/traffic_signs/ES/ES_R2.png',
@@ -228,7 +228,7 @@ style: function (feature) {
 							image: new ol.style.Icon({
 							src: 'https://raw.githubusercontent.com/yopaseopor/beta_preset_josm/master/ES/traffic_signs/ES/ES_R2.png',
 							rotation:0,
-							scale:0.20
+							scale:0.30
 						})
 						})
 					},
@@ -239,7 +239,7 @@ style: function (feature) {
 							rotation:270,
 							offsetX : 20,
 							offsetY : 30,
-							scale:0.20
+							scale:0.30
 						})
 						})
 					},
