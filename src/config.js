185ced43ae6d278a1e20c8413b7629c6d41a4fc0
yopaseopor <yopaseopor@gmail.com>
2024-04-21 16:52:14 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -3510,7 +3510,7 @@ var config = {
        src: 'https://raw.githubusercontent.com/yopaseopor/beta_preset_josm/master/ES/traffic_signs/ES/ES_P1.svg',
        rotation:0,
 	   rotateWithView: true,
-	   anchor: [30,30],
+	   anchor: [3,3],
        scale: 0.25
        }),
 text: new ol.style.Text({
