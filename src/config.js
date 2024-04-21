fb9d362fcc2fa8cbbbd0f63cadcf9ebe47e83064
yopaseopor <yopaseopor@gmail.com>
2024-04-21 16:04:29 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -3509,9 +3509,9 @@ var config = {
 		image: new ol.style.Icon({
        src: 'https://raw.githubusercontent.com/yopaseopor/beta_preset_josm/master/ES/traffic_signs/ES/ES_P1.svg',
        rotation:0,
-	   offsetX : 3,
+	   offsetX : 30,
 	   offsetY : 0,
-       scale: 0.10
+       scale: 0.25
        }),
 text: new ol.style.Text({
 								text: name,
