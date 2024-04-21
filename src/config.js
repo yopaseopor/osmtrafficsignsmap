843cd484b7ecdfeab0dcaf413c91984ccd4dfd2b
yopaseopor <yopaseopor@gmail.com>
2024-04-21 22:25:32 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -3476,7 +3476,7 @@ var config = {
 },
 {
 group: 'ES',
-title: 'ES:P1',
+title: 'ES:P1-',
 query: '(nwr["traffic_sign"="ES:P1"]({{bbox}});node(w););out meta;',
 iconSrc:'https://raw.githubusercontent.com/yopaseopor/beta_preset_josm/master/ES/traffic_signs/ES/ES_P1.svg',
 iconStyle: 'background-color:rgba(255,255,255,0.4)',
