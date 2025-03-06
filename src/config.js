fdbaf3bee35c61c6401ff75f02880633c46bf9c8
yopaseopor <yopaseopor@gmail.com>
2025-03-06 18:26:13 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -429,7 +429,7 @@ iconSrc:'https://raw.githubusercontent.com/yopaseopor/beta_preset_josm/master/ES
 
 		{
 			group: 'Test',
-			title: 'ES_CAT_URB4',
+			title: 'ES_CAT_URB5',
 query: '(nwr["direction"="forward"]({{bbox}});node(w););out meta;',
 iconSrc:'https://raw.githubusercontent.com/yopaseopor/beta_preset_josm/master/ES/traffic_signs/ES/ES_CAT_URB.png',
 			style: function (feature) {
