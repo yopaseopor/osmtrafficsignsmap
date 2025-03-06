66dc8e22309fc1799e3e488b9a95782c2ae3e634
yopaseopor <yopaseopor@gmail.com>
2025-03-06 20:47:29 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -281,13 +281,16 @@ iconSrc:'https://raw.githubusercontent.com/yopaseopor/beta_preset_josm/master/ES
 
 		{
 			group: 'Test',
-			title: 'ES_CAT_URB14',
+			title: 'ES_CAT_URB15',
 query: '(nwr["direction"="forward"]({{bbox}});node(w););out meta;',
 iconSrc:'https://raw.githubusercontent.com/yopaseopor/beta_preset_josm/master/ES/traffic_signs/ES/ES_CAT_URB.png',
 			style: function (feature) {
 				var key_regex = /^direction$/
 				var name_key = feature.getKeys().filter(function(t){return t.match(key_regex)}).pop() || "name"
 				var name = feature.get(name_key) || '';
+				var key_regex2 = /^destination:ref$/
+				var name_key2 = feature.getKeys().filter(function(t){return t.match(key_regex2)}).pop() || "name2"
+				var name2 = feature.get(name_key2) || '';
 				var styles = {
 					'amenity': {
 						'parking': new ol.style.Style({
