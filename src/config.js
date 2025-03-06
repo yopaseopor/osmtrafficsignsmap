1634dd67e5cc468109042980a143d8eeb4846754
yopaseopor <yopaseopor@gmail.com>
2025-03-06 22:04:48 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -697,8 +697,8 @@ document.body.appendChild(canvas2);
 					image: new ol.style.Icon({
 					img: canvas2,
 					imgSize: [200, 150],
-					offsetX : 7,
-							offsetY : -13,
+					offsetX : 30,
+							offsetY : -200,
 						fill: fill,
 						stroke: stroke,
 								}),
