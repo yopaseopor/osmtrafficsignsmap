534791046476da243764fe7191728d80349f0727
yopaseopor <yopaseopor@gmail.com>
2025-03-08 22:12:54 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -731,7 +731,7 @@ ctx2.fillRect(0, 0, canvas2.width, canvas2.height);
 				});
 				var style7 = new ol.style.Style({
 					image: new ol.style.Icon({
-							src: imgSrc + 'ES/textures/content/ES_CAT_URB_empty.svg',
+							src: imgSrc + 'ES/textures/ES_CAT_URB_empty.svg',
 							imgSize: [602, 80],
 					anchor: [0, 0],
 					anchorXUnits: 'pixels',
