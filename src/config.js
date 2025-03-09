3ddaea62c090b5debf6e29d50d7dbae583e0affe
yopaseopor <yopaseopor@gmail.com>
2025-03-09 11:05:30 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -736,7 +736,7 @@ iconSrc:'https://raw.githubusercontent.com/yopaseopor/beta_preset_josm/master/ES
 					anchor: [120, 30],
 					anchorXUnits: 'pixels',
 					anchorYUnits: 'pixels',
-							scale:0.55
+							scale:0.52
 						}),
 												
 					stroke: stroke
