7a0966ff07340230733f3c10700430458194657b
yopaseopor <yopaseopor@gmail.com>
2025-03-10 20:35:13 +0100

    test zaragoza

--- src/config.js
+++ src/config.js
@@ -273,58 +273,6 @@ return styles[key][regexp];
 return null;
 }
 },
-{
-group: 'ES',
-title: 'ES:',
-query: '(nwr["traffic_sign"="ES:"]({{bbox}});node(w););out meta;',
-iconSrc:'https://raw.githubusercontent.com/yopaseopor/beta_preset_josm/master/ES/traffic_signs/ES/ES_.png',
-iconStyle: 'background-color:rgba(255,255,255,0.4)',
-style: function (feature) {
-var name = feature.get('name') || '';
-var styles = {
-'direction': {
-'forward':  new ol.style.Style({
-image: new ol.style.Icon({
-src: 'https://raw.githubusercontent.com/yopaseopor/beta_preset_josm/master/ES/traffic_signs/ES/ES_.png',
-rotation:0,
-rotateWithView: false,
-anchor: [1,0],
-scale: 0.50
-}),
-text: new ol.style.Text({
-text: name,
-font: 'small-caps bold 10px/1 sans-serif',
-offsetX : 80,
-offsetY : -4,
-fill: new ol.style.Fill({
-color: 'rgba(0,0,0,1)'
-})
-})
-}),
-'backward':  new ol.style.Style({
-image: new ol.style.Icon({
-src: 'https://raw.githubusercontent.com/yopaseopor/beta_preset_josm/master/ES/traffic_signs/ES/ES_.png',
-rotation:3.14,
-rotateWithView: false,
-anchor: [-1,0],
-scale: 0.50
-})
-})
-}
-};
-for (var key in styles) {
-var value = feature.get(key);
-if (value !== undefined) {
-for (var regexp in styles[key]) {
-if (new RegExp(regexp).test(value)) {
-return styles[key][regexp];
-}
-}
-}
-}
-return null;
-}
-},
   
 {
 			group: 'Generic',
@@ -421,6 +369,212 @@ return null;
 				});
 				
 				return [style];
+			}
+
+},
+  
+{
+			group: 'ES',
+			title: 'ES_S700',
+			query: '(nwr["traffic_sign"="ES:S7.."]({{bbox}});node(w););out meta;',
+			iconSrc:'https://raw.githubusercontent.com/yopaseopor/beta_preset_josm/master/ES/traffic_signs/ES/ES_S700.png',
+			iconStyle: 'background-color:#FFFFFF',
+			style: function (feature) {
+				var key_regex = /^destination$/
+				var name_key = feature.getKeys().filter(function(t){return t.match(key_regex)}).pop() || "name"
+				var name = feature.get(name_key) || '';
+				var key_regex2 = /^destination:ref$/
+				var name_key2 = feature.getKeys().filter(function(t){return t.match(key_regex2)}).pop() || "name2"
+				var name2 = feature.get(name_key2) || '';
+				var key_regex4 = /^turn:destination$/
+				var name_key4 = feature.getKeys().filter(function(t){return t.match(key_regex4)}).pop() || "name4"
+				var name4 = feature.get(name_key4) || '';
+				var key_regex3 = /^colour:ref$/
+				var name_key3 = feature.getKeys().filter(function(t){return t.match(key_regex3)}).pop() || "name3"
+				var name3 = feature.get(name_key3) || '';
+				var key_regex5 = /^destination:symbol$/
+				var name_key5 = feature.getKeys().filter(function(t){return t.match(key_regex5)}).pop() || "name5"
+				var name5 = feature.get(name_key5) || '';
+				var key_regex6 = /^colour:back$/
+				var name_key6 = feature.getKeys().filter(function(t){return t.match(key_regex6)}).pop() || "name6"
+				var name6 = feature.get(name_key6) || '';
+				var key_regex7 = /^colour:text$/
+				var name_key7 = feature.getKeys().filter(function(t){return t.match(key_regex7)}).pop() || "name7"
+				var name7 = feature.get(name_key7) || '';
+				var key_regex8 = /^colour:ref:2$/
+				var name_key8 = feature.getKeys().filter(function(t){return t.match(key_regex8)}).pop() || "name8"
+				var name8 = feature.get(name_key8) || '';
+				var key_regex9 = /^destination:ref:2$/
+				var name_key9 = feature.getKeys().filter(function(t){return t.match(key_regex9)}).pop() || "name9"
+				var name9 = feature.get(name_key9) || '';
+				var key_regex10 = /^colour:ref:3$/
+				var name_key10 = feature.getKeys().filter(function(t){return t.match(key_regex10)}).pop() || "name10"
+				var name10 = feature.get(name_key10) || '';
+				var key_regex11 = /^destination:ref:3$/
+				var name_key11 = feature.getKeys().filter(function(t){return t.match(key_regex11)}).pop() || "name11"
+				var name11 = feature.get(name_key11) || '';
+				
+				var canvas = document.createElement('canvas');
+				canvas.width = 310;
+				canvas.height = 20;
+				var ctx = canvas.getContext('2d');
+				ctx.fillStyle = name6;
+				ctx.fillRect(0, 0, canvas.width, canvas.height);
+				
+				/*var fill = new ol.style.Fill({
+					color: name3
+				});*/
+
+				var stroke = new ol.style.Stroke({
+					color: '#00FF00',
+					width: 1.25
+				});
+				var style = new ol.style.Style({
+					text: new ol.style.Text({
+								text: name,
+								font: 'bold 10px Verdana',
+								textAlign:'left',
+								offsetX : 20,
+								offsetY : 7,
+								rotateWithView: false,
+								fill: new ol.style.Fill({
+                            color: name7
+                        }),
+							}),
+					
+					stroke: stroke
+				});
+				var style2 = new ol.style.Style({
+												text: new ol.style.Text({
+								text: name2,
+								font: 'bolder 12px Arial',
+								textAlign:'center',
+								offsetX : -3,
+								offsetY : 28,
+								rotateWithView: false,
+								fill: new ol.style.Fill({
+                            color: 'rgba(255,255,250,1)'
+                        }),
+							}),
+					
+					stroke: stroke
+				});
+				var style9 = new ol.style.Style({
+												text: new ol.style.Text({
+								text: name9,
+								font: 'bolder 12px Arial',
+								textAlign:'center',
+								offsetX : 65,
+								offsetY : 28,
+								rotateWithView: false,
+								fill: new ol.style.Fill({
+                            color: 'rgba(255,255,250,1)'
+                        }),
+							}),
+					
+					stroke: stroke
+				});
+				var style11 = new ol.style.Style({
+												text: new ol.style.Text({
+								text: name11,
+								font: 'bolder 12px Arial',
+								textAlign:'center',
+								offsetX : 135,
+								offsetY : 28,
+								rotateWithView: false,
+								fill: new ol.style.Fill({
+                            color: 'rgba(255,255,250,1)'
+                        }),
+							}),
+					
+					stroke: stroke
+				});
+				var style3 = new ol.style.Style({
+					image: new ol.style.Icon({
+							src: imgSrc + 'ES/textures/content/rectangle_small_'+ name3 +'.png',
+					imgSize: [50, 15],
+					rotateWithView: false,
+					anchor: [30, -20],
+					anchorXUnits: 'pixels',
+					anchorYUnits: 'pixels'
+								}),
+					/*fill: fill,*/
+					stroke: stroke
+				});
+				var style8 = new ol.style.Style({
+					image: new ol.style.Icon({
+							src: imgSrc + 'ES/textures/content/rectangle_small_'+ name8 +'.png',
+					imgSize: [50, 15],
+					rotateWithView: false,
+					anchor: [-40, -20],
+					anchorXUnits: 'pixels',
+					anchorYUnits: 'pixels'
+								}),
+					/*fill: fill,*/
+					stroke: stroke
+				});
+				var style10 = new ol.style.Style({
+					image: new ol.style.Icon({
+							src: imgSrc + 'ES/textures/content/rectangle_small_'+ name10 +'.png',
+					imgSize: [50, 15],
+					rotateWithView: false,
+					anchor: [-110, -20],
+					anchorXUnits: 'pixels',
+					anchorYUnits: 'pixels'
+								}),
+					/*fill: fill,*/
+					stroke: stroke
+				});
+				var style4 = new ol.style.Style({
+					image: new ol.style.Icon({
+					img: canvas,
+					imgSize: [310, 20],
+					rotateWithView: false,
+					anchor: [60, 3],
+					anchorXUnits: 'pixels',
+					anchorYUnits: 'pixels'
+								}),
+					/*fill: fill,*/
+					stroke: stroke
+				});
+				var style5 = new ol.style.Style({
+					image: new ol.style.Icon({
+							src: imgSrc + 'ES/textures/content/black_'+ name4 +'_city.png',
+							anchor: [280, 0],
+							rotateWithView: false,
+								anchorXUnits: 'pixels',
+								anchorYUnits: 'pixels',
+							scale:0.20
+						}),
+												
+					stroke: stroke
+				});
+				var style6 = new ol.style.Style({
+					image: new ol.style.Icon({
+							src: imgSrc + 'ES/textures/content/sign_black_'+ name5 +'.png',
+							anchor: [-1150, 0],
+							rotateWithView: false,
+								anchorXUnits: 'pixels',
+								anchorYUnits: 'pixels',
+							scale:0.15
+						}),
+												
+					stroke: stroke
+				});
+				var style7 = new ol.style.Style({
+					image: new ol.style.Icon({
+							src: imgSrc + 'ES/textures/ES_CAT_URBL_empty.svg',
+							imgSize: [602, 154],
+					anchor: [120, 30],
+					rotateWithView: false,
+					anchorXUnits: 'pixels',
+					anchorYUnits: 'pixels',
+							scale:0.52
+						}),
+												
+					stroke: stroke
+				});
+				return [style, style2, style9, style11, style7, style4, style3, style8, style10, style5, style6];
 			}
 
 },
