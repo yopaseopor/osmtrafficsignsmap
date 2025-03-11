9680b907d862299a36ee30bb8fc3b6820961d808
yopaseopor <yopaseopor@gmail.com>
2025-03-11 21:49:42 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -453,7 +453,7 @@ return null;
 								font: 'bolder 12px Arial',
 								textAlign:'center',
 								offsetX : 195,
-								offsetY : 12,
+								offsetY : 15,
 								rotateWithView: false,
 								fill: new ol.style.Fill({
                             color: 'rgba(255,255,250,1)'
@@ -468,7 +468,7 @@ return null;
 								font: 'bolder 12px Arial',
 								textAlign:'center',
 								offsetX : 195,
-								offsetY : 30,
+								offsetY : 35,
 								rotateWithView: false,
 								fill: new ol.style.Fill({
                             color: 'rgba(255,255,250,1)'
@@ -483,7 +483,7 @@ return null;
 								font: 'bolder 12px Arial',
 								textAlign:'center',
 								offsetX : 195,
-								offsetY : 50,
+								offsetY : 55,
 								rotateWithView: false,
 								fill: new ol.style.Fill({
                             color: 'rgba(255,255,250,1)'
