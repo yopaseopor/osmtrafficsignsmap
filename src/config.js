f46d8eb8fecc4f49a7fcc1bb5f98af9124e6dbde
yopaseopor <yopaseopor@gmail.com>
2025-03-12 20:11:56 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1606,7 +1606,7 @@ ctx2.fillRect(0, 0, canvas2.width, canvas2.height);
 				var style10 = new ol.style.Style({
 					image: new ol.style.Icon({
 							src: imgSrc + 'ES/textures/content/roundabout_left_'+ word10 +'.png',
-							anchor: [-100, -40],
+							anchor: [-100, -60],
 								anchorXUnits: 'pixels',
 								anchorYUnits: 'pixels',
 							scale:1
