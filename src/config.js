368814491949faaf972e49cab599389b6d0bf9ef
yopaseopor <yopaseopor@gmail.com>
2025-03-12 19:38:34 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1605,7 +1605,7 @@ ctx2.fillRect(0, 0, canvas2.width, canvas2.height);
 				});
 				var style10 = new ol.style.Style({
 					image: new ol.style.Icon({
-							src: imgSrc + 'ES/textures/content/roundabout_'+ word10 +'.png',
+							src: imgSrc + 'ES/textures/content/roundabout_left_'+ word10 +'.png',
 							anchor: [0, 0],
 								anchorXUnits: 'pixels',
 								anchorYUnits: 'pixels',
