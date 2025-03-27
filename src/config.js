1591a2e145fcfdd9e5ff66261098712687a4dfb0
yopaseopor <yopaseopor@gmail.com>
2025-03-27 20:44:49 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2792,7 +2792,7 @@ ctx2.fillRect(0, 0, canvas2.width, canvas2.height);
 							src: imgSrc + 'ES/textures/content/rectangle_small_'+ word3 +'.png',
 					imgSize: [50, 15],
 					rotateWithView: false,
-					anchor: [-38, 23],
+					anchor: [-37, 23],
 					anchorXUnits: 'pixels',
 					anchorYUnits: 'pixels'
 								}),
