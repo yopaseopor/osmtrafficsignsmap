0decc1e12aa987162802ee2cd41e5d3875e8df70
yopaseopor <yopaseopor@gmail.com>
2025-03-28 23:28:43 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -3100,6 +3100,7 @@ ctx2.fillRect(0, 0, canvas2.width, canvas2.height);
 						}),
 												
 					stroke: stroke
+					});
 				var style16 = new ol.style.Style({
 					image: new ol.style.Icon({
 							src: imgSrc + 'ES/textures/content/sign_'+ word26 +'_'+ word21 +'.png',
