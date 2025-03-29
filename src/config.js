091f2d8e69868e401368cecf344bf977f766dfe2
yopaseopor <yopaseopor@gmail.com>
2025-03-29 12:39:46 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1952,7 +1952,7 @@ iconSrc:'https://raw.githubusercontent.com/yopaseopor/beta_preset_josm/master/ES
 				var word75c = feature.get(word_key75c) || '';
 				var key_regex76 = /^colour:back:right$/
 				var word_key76 = feature.getKeys().filter(function(t){return t.match(key_regex76)}).pop() || "word76"
-				var word76 = feature.get(word_key76) || '';
+				var word76 = feature.get(word_key76) || undefined;
 				var key_regex77 = /^colour:text:right$/
 				var word_key77 = feature.getKeys().filter(function(t){return t.match(key_regex77)}).pop() || "word77"
 				var word77 = feature.get(word_key77) || '';
@@ -2218,13 +2218,13 @@ ctx2.fillRect(0, 0, canvas2.width, canvas2.height);
 								offsetY : 125,
 								fill: new ol.style.Fill({
                             ccolor: word77
-                         }),
-        padding: [0,1,0,1],
-        backgroundFill: typeof word76 !== 'undefined' ? new ol.style.Fill({
-            color: word76
-        }) : undefined,
-    }),
-    stroke: stroke
+                        }),
+							padding: [0,1,0,1],
+								backgroundFill: new ol.style.Fill({
+                            color: word76
+                        }),
+					}),
+					stroke: stroke
 				});
 					var style70b = new ol.style.Style({
 					text: new ol.style.Text({
