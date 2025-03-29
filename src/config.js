89425f57b66e5cbb2a9e78da72532326e1abfde2
yopaseopor <yopaseopor@gmail.com>
2025-03-29 11:42:45 +0100

    test

--- src/config.js
+++ src/config.js
@@ -2017,7 +2017,7 @@ ctx2.fillRect(0, 0, canvas2.width, canvas2.height);
                         }),
 							padding: [0,1,0,1],
 								backgroundFill: new ol.style.Fill({
-                            color: word6
+                            color: word6 || 'rgba(248,248,255,1)'
                         }),
 					}),
 					stroke: stroke
