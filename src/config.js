62565d7367fd46fbf2e5e65ebf8a78cd9195c998
yopaseopor <yopaseopor@gmail.com>
2025-03-30 15:14:12 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -49407,9 +49407,9 @@ ctx2.fillRect(0, 0, canvas2.width, canvas2.height);
 				var style6 = new ol.style.Style({
 					image: new ol.style.Icon({
 							src: imgSrc + 'ES/textures/content/colour_back_medium_'+ word6 +'.png',
-					imgSize: [117, 90],
+					imgSize: [130, 100],
 					rotateWithView: false,
-					anchor: [-115, -2],
+					anchor: [-130, -2],
 					anchorXUnits: 'pixels',
 					anchorYUnits: 'pixels'
 								}),
