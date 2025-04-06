44a5465800b3a6320c927c7be3efa3aa038e56e5
yopaseopor <yopaseopor@gmail.com>
2025-04-06 21:29:24 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -4315,7 +4315,7 @@ ctx2.fillRect(0, 0, canvas2.width, canvas2.height);
 				var style35 = new ol.style.Style({
 					image: new ol.style.Icon({
 							src: imgSrc + 'ES/textures/content/sign_white_'+ word35 +'.png',
-							anchor: [-300, -750],
+							anchor: [-350, -800],
 							rotateWithView: false,
 								anchorXUnits: 'pixels',
 								anchorYUnits: 'pixels',
@@ -4352,7 +4352,7 @@ ctx2.fillRect(0, 0, canvas2.width, canvas2.height);
 				var style45 = new ol.style.Style({
 					image: new ol.style.Icon({
 							src: imgSrc + 'ES/textures/content/sign_white_'+ word45 +'.png',
-							anchor: [-350, -1250],
+							anchor: [-300, -1350],
 							rotateWithView: false,
 								anchorXUnits: 'pixels',
 								anchorYUnits: 'pixels',
