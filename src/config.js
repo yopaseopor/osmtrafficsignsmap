31c6ba7ead29442edef6baab51d4e9e02be6e757
yopaseopor <yopaseopor@gmail.com>
2025-04-06 21:18:35 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -4377,7 +4377,7 @@ ctx2.fillRect(0, 0, canvas2.width, canvas2.height);
 				var style45c = new ol.style.Style({
 					image: new ol.style.Icon({
 							src: imgSrc + 'ES/textures/content/sign_white_'+ word45c +'.png',
-							anchor: [-550, -1450],
+							anchor: [-650, -1350],
 							rotateWithView: false,
 								anchorXUnits: 'pixels',
 								anchorYUnits: 'pixels',
