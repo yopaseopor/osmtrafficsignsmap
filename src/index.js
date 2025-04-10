809f8ebb409f951efea546a6fc9914f8ff981765
yopaseopor <yopaseopor@gmail.com>
2025-04-10 23:39:44 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ $(function () {
 	$('#map').empty(); // Remove Javascript required message
 	var baseLayerIndex = 0;
 	
+	
+	
 
 	//Object to manage the spinner layer
 	var loading = {
@@ -382,6 +384,56 @@ $(function () {
 	map.addControl(new ol.control.Control({
 		element: permalinkControlBuild()
 	}));
+	
+// Set the search control 
+var searchNominatim = new ol.control.SearchNominatim (
+{   
+    //  polygon: $("#polygon").prop("checked"),
+        reverse: true,
+        polygon: false,
+        position: true, // Search, with priority to geo position
+        title: "Busca",
+        reverseTitle: "Clique no mapa...",
+        placeholder: "Busca...",
+//      target: $("#address > div").get(0)
+});
+var requestData = searchNominatim.requestData.bind(searchNominatim);
+searchNominatim.requestData = function (s) {
+    var data = requestData(s);
+    data.countrycodes = 'br';
+    data.citt
+    return data;
+};
+map.addControl (searchNominatim);
+
+// Select feature when click on the reference index
+searchNominatim.on('select', function(e)
+{   
+//  sLayer.getSource().clear();
+    // Check if we get a geojson to describe the search
+    if (e.search.geojson) {
+        var format = new ol.format.GeoJSON();
+        var f = format.readFeature(e.search.geojson, { dataProjection: "EPSG:4326", featureProjection: map.getView().getProjection() });
+    //  sLayer.getSource().addFeature(f);
+        var view = map.getView();
+        var resolution = view.getResolutionForExtent(f.getGeometry().getExtent(), map.getSize());
+        var zoom = view.getZoomForResolution(resolution);
+        var center = ol.extent.getCenter(f.getGeometry().getExtent());
+        // redraw before zoom
+        setTimeout(function(){
+                view.animate({
+                center: center,
+                zoom: Math.min (zoom, 16)
+            });
+        }, 100);
+    }
+    else {
+        map.getView().animate({
+            center:e.coordinate,
+            zoom: Math.max (map.getView().getZoom(),16)
+        });
+    }
+});
 
 	// Rotate left button
 	var rotateleftControlBuild = function () {
