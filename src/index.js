37ae47565868101fc6dc7b8c1bd46dc6121f540b
yopaseopor <yopaseopor@gmail.com>
2025-04-10 23:17:30 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,44 @@
 $(function () {
 	$('#map').empty(); // Remove Javascript required message
 	var baseLayerIndex = 0;
+	
+	// Set the search control 
+  var search = new ol.control.SearchNominatim({
+    //target: $(".options").get(0),
+    polygon: $("#polygon").prop("checked"),
+    reverse: true,
+    position: true	// Search, with priority to geo position
+  });
+  map.addControl (search);
+
+  // Select feature when click on the reference index
+  search.on('select', function(e) {
+    // console.log(e);
+    sLayer.getSource().clear();
+    // Check if we get a geojson to describe the search
+    if (e.search.geojson) {
+      var format = new ol.format.GeoJSON();
+      var f = format.readFeature(e.search.geojson, { dataProjection: "EPSG:4326", featureProjection: map.getView().getProjection() });
+      sLayer.getSource().addFeature(f);
+      var view = map.getView();
+      var resolution = view.getResolutionForExtent(f.getGeometry().getExtent(), map.getSize());
+      var zoom = view.getZoomForResolution(resolution);
+      var center = ol.extent.getCenter(f.getGeometry().getExtent());
+      // redraw before zoom
+      setTimeout(function(){
+        view.animate({
+        center: center,
+        zoom: Math.min (zoom, 16)
+      });
+      }, 100);
+    } else {
+      map.getView().animate({
+        center:e.coordinate,
+        zoom: Math.max (map.getView().getZoom(),16)
+      });
+    }
+  });
+
 
 	//Object to manage the spinner layer
 	var loading = {
